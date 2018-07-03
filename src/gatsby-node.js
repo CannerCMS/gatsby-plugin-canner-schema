0d04017dc08f0b09dd8ff7c5dbf29155133cb021
chilijung <chilijung@gmail.com>
2018-07-03 15:37:09 +0800

    fix preset settings

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -26,7 +26,7 @@ exports.onCreateWebpackConfig = (
               presets: [
                 require('@babel/preset-env'),
                 require('@babel/preset-react'),
-                require('@babel/preset-stage-0'),
+                [require('@babel/preset-stage-0'), { "decoratorsLegacy": true }],
               ]
             }
           }
