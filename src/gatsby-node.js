03d51812115b052edd150d3028d8c9f835968695
chilijung <chilijung@gmail.com>
2018-07-03 15:12:17 +0800

    add babel settings and options

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -11,7 +11,25 @@ exports.onCreateWebpackConfig = (
         test: /\.schema\.js|canner\.def\.js$/,
         use: [
           {loader: resolve('canner-schema-loader')},
-          {loader: resolve('babel-loader')}
+          {
+            loader: resolve('babel-loader'),
+            options: {
+              plugins: [
+                [
+                  require('babel-plugin-import').default,
+                  {
+                    libraryName: 'antd',
+                    style: true,
+                  },
+                ],
+              ],
+              presets: [
+                require('babel-preset-env'),
+                require('babel-preset-react'),
+                require('babel-preset-stage-0'),
+              ]
+            }
+          }
         ],
       }],
     },
